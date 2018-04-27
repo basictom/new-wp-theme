511c2c8b6ed2ec5c27db92c1f5f386cd2055b4fe
Thomas Fitzgerald <tpfitz42@gmail.com>
2018-04-26 21:32:44 -0500

    added some more styles

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,1 +1,8 @@
-skrollr.init()
+var nav = document.getElementById('header-navigation')
+
+if (window.matchMedia("(min-width: 768px)").matches) {
+  skrollr.init()
+} else {
+  console.log("screen width is less than 768px. This is where I am going to output the styles for the hamburger navigation")
+  nav.setAttribute('style', 'display:none;');
+}
